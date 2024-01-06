04d8de118e50b01608f3aa56f7c6784485371e1a
Blaspace <115324109+Blaspace@users.noreply.github.com>
2024-01-06 04:32:46 -0800

    ll

--- controler/user.js
+++ controler/user.js
@@ -4,7 +4,10 @@ const handleGetUser = (req, res) => {
   const refreshtoken = req.cookies.jwt;
 
   User.findOne({ refreshtoken })
-    .then((data) => res.send(data))
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
     .catch((err) => res.sendStatus(400));
 };
 module.exports = handleGetUser;
